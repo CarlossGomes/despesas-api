f6bd604b8ca440108fdcb1a838d7f2778b8a9b21
CarlossGomes <carlosdaniel_ag@hotmail.com>
2021-01-30 15:41:34 -0300

    Atualizacao

--- src/main/java/com/example/despesas/api/repository/UsuarioRepository.java
+++ src/main/java/com/example/despesas/api/repository/UsuarioRepository.java
@@ -3,9 +3,11 @@ package com.example.despesas.api.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.despesas.api.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Optional<Usuario> findByEmail(String email);
